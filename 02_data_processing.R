685617d66eb930bd57996be50110f87929769328
Nigel Rimando <nigel.rimando@firstcircle.com>
2020-05-24 17:33:32 +0800

    processing

--- 02_data_processing.R
+++ 02_data_processing.R
@@ -6,10 +6,32 @@ path_folder <- "data/documents_processed/"
 
 # Data Processing ---------------------------------------------------------
 
+# b9211
+
 magick_split_pdf(
-  file_name = glue::glue(
-    folder_path, "b9211_la9132_a17927.pdf"
-  ),
-  retained_pages = c(1, 3, 5, 7, 9, 11),
-  path_folder = path_folder
+  file_name = glue::glue(folder_path, "b9211_la9132_a17927.pdf"),
+  retained_pages = c(1, 3, 5, 7, 9, 11), path_folder = path_folder
+)
+
+for (i in c(2748, 2746)) {
+  magick_split_pdf(
+    file_name = glue::glue(folder_path, "b9211_la5589_a{i}.pdf"),
+    retained_pages = c(1), path_folder = path_folder
+  )
+}
+
+for (i in c(9738, 9737, 9736, 9735, 9734, 9733, 9732, 9731, 9730, 9729,
+            9728, 9725, 9723, 9722)) {
+  magick_split_pdf(
+    file_name = glue::glue(folder_path, "b9211_la20247_a{i}.pdf"),
+    retained_pages = c(1), path_folder = path_folder
+  )
+}
+
+# Skipped
+# la12848_a7085
+
+magick_split_pdf(
+  file_name = glue::glue(folder_path, "b9211_la12848_a7084.pdf"),
+  retained_pages = 1:13, path_folder = path_folder, degrees = 90
 )
