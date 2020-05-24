b4aae7dfda9ad60a52068fc6243a53923b5d385a
Nigel Rimando <nigel.rimando@firstcircle.com>
2020-05-24 17:07:20 +0800

    magick_split_pdf

---
+++ 01_data_pulling.R
@@ -0,0 +1,52 @@
+
+
+# Ran and downloaded from po_links.sql
+buyer_loanapp.dt <- read_csv("data/buyers_loanapps.csv") %>%
+  arrange(buyer_id, loan_application_id, attachment_id)
+
+library(stat299capstone)
+library(magrittr)
+
+s3 <- paws::s3()
+
+# Get One Document --------------------------------------------------------
+
+loan_application_id <- buyer_loanapp.dt$loan_application_id[1]
+attachment_id <- buyer_loanapp.dt$attachment_id[1]
+buyer_id <- buyer_loanapp.dt$buyer_id[1]
+
+s3 %>%
+  s3_get_document(
+    loan_application_id = loan_application_id,
+    attachment_id = attachment_id
+  ) %$%
+  Body %>%
+  magick_render_pdf(
+    path = glue::glue(
+      "data/documents/b{buyer_id}_la{loan_application_id}_a{attachment_id}.pdf"
+    )
+  )
+
+# Looping -----------------------------------------------------------------
+
+filtered_dataset <- buyer_loanapp.dt %>%
+  filter(buyer_id == 9211)
+
+for (i in 1:nrow(filtered_dataset)) {
+  loan_application_id <- filtered_dataset$loan_application_id[i]
+  attachment_id <- filtered_dataset$attachment_id[i]
+  buyer_id <- filtered_dataset$buyer_id[i]
+
+  s3 %>%
+    s3_get_document(
+      loan_application_id = loan_application_id,
+      attachment_id = attachment_id
+    ) %$%
+    Body %>%
+    magick_render_pdf(
+      path = glue::glue(
+        "data/documents/b{buyer_id}_la{loan_application_id}_a{attachment_id}.pdf"
+      )
+    )
+}
+
---
+++ R/magick_split_pdf.R
@@ -0,0 +1,37 @@
+#' Split PDF into separate pages
+#'
+#' @param file_name path to pdf
+#' @param retained_pages pages in the pdf to capture and retain
+#' @param path_folder folder to dump results
+#'
+#' @import magick pdftools
+#' @importFrom stringr str_replace
+#' @importFrom assertthat assert_that
+#' @importFrom glue glue
+#'
+#' @export
+#'
+#' @examples
+#' magick_split_pdf(file_name = "data/documents/b9211_la9132_a17927.pdf", retained_pages = c(1, 3, 5, 7, 9, 11), path_folder = "data/documents_processed")
+magick_split_pdf <- function(file_name, retained_pages, path_folder) {
+  for (i in retained_pages) {
+
+    x <- image_read_pdf(file_name, pages = i)
+
+    new_file_name <- str_replace(
+      str_replace(file_name,  ".pdf", ""),
+      "documents",
+      "documents_processed"
+    )
+
+    x %>%
+      image_write(
+        path = glue::glue(
+          "{new_file_name}_page{i}.pdf"
+        ),
+        format = "pdf"
+      )
+
+  }
+}
+
